08ff499558b4740a72800bf0fc5d373f415329d2
Furkan OGUZGOKSU <furkanoguz4242@hotmail.com>
2025-07-03 15:13:31 +0300

    Main UI title changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Book App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black54),
