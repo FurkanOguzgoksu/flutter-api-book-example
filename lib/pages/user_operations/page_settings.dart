15013e696085a1c4e5b96a409fe103a36ce4153b
Furkan OGUZGOKSU <furkanoguz4242@hotmail.com>
2025-07-10 22:42:21 +0300

    added new page settings

---
+++ lib/pages/user_operations/page_settings.dart
@@ -0,0 +1,36 @@
+import 'package:film_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PageSettings extends StatelessWidget {
+  const PageSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text("Ayarlar"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: ListTile(
+            title: const Text("Tema Modu"),
+            trailing: Switch(
+              value: themeProvider.isDarkMode,
+              onChanged: (bool value) {
+                themeProvider.toggleTheme(value);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
