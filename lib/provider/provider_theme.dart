e98a362164dc230290d8cd8ea0308089e0dd3b13
Furkan OGUZGOKSU <furkanoguz4242@hotmail.com>
2025-07-12 11:37:18 +0300

    added theme provider

---
+++ lib/provider/provider_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool isDarkMode = false;
+
+  ThemeProvider(this.isDarkMode);
+
+  void toggleTheme(bool value) {
+    isDarkMode = value;
+    notifyListeners();
+
+    // Kalıcı olarak kaydetme
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setBool('isDarkMode', value);
+    });
+  }
+}
