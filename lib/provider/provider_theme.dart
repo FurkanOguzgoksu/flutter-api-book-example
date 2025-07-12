25f9e69f7f8fc1c9a30cd1d6b03a55717ca42ac0
Furkan OGUZGOKSU <furkanoguz4242@hotmail.com>
2025-07-12 13:54:18 +0300

    added provider theme package

--- lib/provider/provider_theme.dart
+++ lib/provider/provider_theme.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider extends ChangeNotifier {
-  bool isDarkMode = false;
-
-  ThemeProvider(this.isDarkMode);
-
-  void toggleTheme(bool value) {
-    isDarkMode = value;
-    notifyListeners();
-
-    // Kalıcı olarak kaydetme
-    SharedPreferences.getInstance().then((prefs) {
-      prefs.setBool('isDarkMode', value);
-    });
-  }
-}
