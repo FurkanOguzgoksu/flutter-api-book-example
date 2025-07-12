55076db2f270e958f2e62edbd632ea7ebde01aea
Furkan OGUZGOKSU <furkanoguz4242@hotmail.com>
2025-07-12 13:41:12 +0300

    update colors became global and design changes

--- lib/pages/page_confirm.dart
+++ lib/pages/page_confirm.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:film_app/features_personal/user_model.dart';
 import 'package:film_app/pages/page_home.dart';
 import 'package:film_app/provider/provider_basket.dart';
+import 'package:film_app/widgets/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -78,98 +79,143 @@ class _PageConfirmState extends State<PageConfirm> {
     final random = Random();
     String orderNo = (1000 + random.nextInt(9000)).toString();
     return SingleChildScrollView(
-      child: Card(
-        color: widget.success == true ? Colors.green : Colors.red,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        elevation: 8,
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              Icon(
-                widget.success == true ? Icons.check_circle : Icons.error,
-                size: 60,
-                color: Colors.white,
-              ),
-              const SizedBox(height: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Text(
-                      widget.success == true
-                          ? "Sayın ${widget.personal?.username ?? "Bilinmeyen Kullanıcı"},\nSiparişiniz alınmıştır!"
-                          : "Siparişiniz başarısız oldu!",
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  if (widget.success == true) ...[
-                    const SizedBox(height: 15),
-                    Text(
-                      "Sipariş No: $orderNo\n"
-                      "Sipariş Tarihi: $todaysDate\n"
-                      "Toplam Ürün Sayısı: ${basket.totalItems} adet\n"
-                      "Tahmini Teslimat: 3 iş günü\n"
-                      "Teslimat Tipi: ${widget.paymetMethod}\n"
-                      "Teslimat Adresi: ${widget.adress}\n"
-                      "Ödenen Tutar: ${NumberFormat.currency(locale: 'tr_TR', symbol: '', decimalDigits: 2).format(basket.totalPrice)} ₺",
-                      style: const TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      "Alınan Ürünler:",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    ...basket.basketBooks.entries.map((entry) {
-                      final book = entry.key;
-                      final int quantity = entry.value;
-                      return Text(
-                        "- ${book.volumeInfo?.title ?? "Ürün"} ($quantity adet)",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                        ),
-                      );
-                    }),
-                  ],
-                  const SizedBox(height: 20),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: widget.success == true
-                          ? () {
-                              basket.deleteBookList();
-                              Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const PageHome(),
-                                ),
-                                (route) => false,
-                              );
-                            }
-                          : () {
-                              Navigator.pop(context);
-                            },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          color: widget.success == true ? Colors.green : Colors.red,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 8,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              children: [
+                Icon(
+                  widget.success == true ? Icons.check_circle : Icons.error,
+                  size: 60,
+                  color: kTextWhiteColor,
+                ),
+                const SizedBox(height: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
                       child: Text(
-                        widget.success == true ? "Anasayfaya Dön" : "Geri Dön",
+                        widget.success == true
+                            ? "Sayın ${widget.personal?.username ?? "Bilinmeyen Kullanıcı"},\nSiparişiniz alınmıştır!"
+                            : "Siparişiniz başarısız oldu!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: kTextWhiteColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    if (widget.success == true) ...[
+                      const SizedBox(height: 15),
+                      Table(
+                        children: [
+                          _confirmTableRow("Sipariş No:", orderNo),
+                          _confirmTableRow("Sipariş Tarihi:", todaysDate),
+                          _confirmTableRow(
+                            "Ürün Sayısı:",
+                            "${basket.totalItems} adet",
+                          ),
+                          _confirmTableRow(
+                            "Ödeme Tipi",
+                            "${widget.paymetMethod}",
+                          ),
+                          _confirmTableRow(
+                            "Ödenen Tutar:",
+                            "${NumberFormat.currency(locale: 'tr_TR', symbol: '', decimalDigits: 2).format(basket.totalPrice)} ₺",
+                          ),
+                          _confirmTableRow("Tahmini Teslimat:", "3 iş günü"),
+                          _confirmTableRow(
+                            "Teslimat Adresi:",
+                            "${widget.adress}",
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 15),
+                      Text(
+                        "Alınan Ürünler:",
+                        style: TextStyle(
+                          color: kTextWhiteColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      ...basket.basketBooks.entries.map((entry) {
+                        final book = entry.key;
+                        final int quantity = entry.value;
+                        return Table(
+                          children: [
+                            _confirmTableRow(
+                              "- ${book.volumeInfo?.title ?? "Ürün"}",
+                              "$quantity adet",
+                            ),
+                          ],
+                        );
+                      }),
+                    ],
+                    const SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: widget.success == true
+                            ? () {
+                                basket.deleteBookList();
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const PageHome(),
+                                  ),
+                                  (route) => false,
+                                );
+                              }
+                            : () {
+                                Navigator.pop(context);
+                              },
+                        child: Text(
+                          widget.success == true
+                              ? "Anasayfaya Dön"
+                              : "Geri Dön",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+TableRow _confirmTableRow(String name, String value) {
+  return TableRow(
+    children: [
+      TableCell(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Text(
+            name,
+            style: TextStyle(color: kTextWhiteColor, fontSize: 16),
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Text(
+          value,
+          style: TextStyle(color: kTextWhiteColor, fontSize: 16),
+        ),
+      ),
+    ],
+  );
 }
